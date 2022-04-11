baefd6e96f138cba1cfb0d39e84c5bf7268a9d32
FaithNzula <faithnzula1230@gmail.com>
2022-04-11 15:19:43 +0300

    imported input API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +25,7 @@ import { DialogueComponent } from './dialogue/dialogue.component';
     MatButtonModule,
     MatDialogModule,
     MatFormFieldModule,
+    MatInputModule,
     
     
     BrowserAnimationsModule
