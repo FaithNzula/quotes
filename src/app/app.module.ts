33ecefad35d27c6eea89ff093a235cb929017705
FaithNzula <faithnzula1230@gmail.com>
2022-04-11 15:13:19 +0300

    added form in dialogue and importe the form API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { DialogueComponent } from './dialogue/dialogue.component';
     MatIconModule,
     MatButtonModule,
     MatDialogModule,
+    MatFormFieldModule,
     
     
     BrowserAnimationsModule
