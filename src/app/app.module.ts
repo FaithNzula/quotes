625aa14e779a82a29fdf78ae0615146aa610e5f9
FaithNzula <faithnzula1230@gmail.com>
2022-04-11 16:06:58 +0300

    added both name input field and quote input field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DialogueComponent } from './dialogue/dialogue.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,8 @@ import { DialogueComponent } from './dialogue/dialogue.component';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    
+    MatSelectModule,
+    MatDatepickerModule,
     
     BrowserAnimationsModule
   ],
