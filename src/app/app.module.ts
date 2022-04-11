7021a48aa7d69779e9a8a6315a093c9580822549
FaithNzula <faithnzula1230@gmail.com>
2022-04-11 14:52:56 +0300

    imported a dialogue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +19,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatDialogModule,
     
     
     BrowserAnimationsModule
