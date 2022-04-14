dd04e0af664264bd71909ecb34c36ac4c7be7fbc
FaithNzula <faithnzula1230@gmail.com>
2022-04-14 23:00:23 +0300

    imported matdialogref

--- src/app/dialogue/dialogue.component.ts
+++ src/app/dialogue/dialogue.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup , FormBuilder , Validators} from '@angular/forms';
 import { ApiService } from '../service/api.service';
-
+import{MatDialogRef} from '@angular/material/dialog'
 @Component({
   selector: 'app-dialogue',
   templateUrl: './dialogue.component.html',
